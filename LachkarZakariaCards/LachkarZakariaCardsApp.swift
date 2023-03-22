9bf474ecc2f8250d057c355338e22b67fd5769c6
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-03-22 11:36:16 +0000

    Initial Commit

---
+++ LachkarZakariaCards/LachkarZakariaCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LachkarZakariaCardsApp.swift
+//  LachkarZakariaCards
+//
+//  Created by zakaria lachkar on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LachkarZakariaCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
